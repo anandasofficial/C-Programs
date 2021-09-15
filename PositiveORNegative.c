5151631bd3df560925ef1ed9e0528ae873151092
Anand AS <78604009+anandasofficial@users.noreply.github.com>
2021-09-15 21:33:31 +0530

    C Program to Check if a given Integer is Positive or Negative
    The program takes the given integer and checks whether the integer is positive or negative.

---
+++ PositiveORNegative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int flag = 1;
+    while(flag)
+    {
+        int number;
+    printf("Enter an Integer:");
+    scanf("%d",&number);
+
+    if(number > 0)
+    {
+        printf("The Given Integer %d is POSITIVE!\n\n",number);
+    }
+    else
+    {
+        printf("The Given Integer %d is NEGATIVE!\n\n",number);
+    }
+
+}
+
+    }
+
