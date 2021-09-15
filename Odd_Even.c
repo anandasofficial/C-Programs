b448982cec390d8a29932d9e9a64c508dc46a3ea
anandasofficial <gitthubofficial@gmail.com>
2021-09-15 20:09:10 +0530

    C Program to Check if a given Integer is Odd or Even

---
+++ Odd_Even.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int flag = 1;
+    while(flag)
+    {
+        int number;
+    printf("Enter the  number:");
+    scanf("%d",&number);
+
+
+    if(number%2 == 0)
+    {
+        printf("The Given Number %d is EVEN!\n",number);
+    }
+    else
+    {
+        printf("The Given Number %d is ODD!\n",number);
+    }
+
+}
+    }
+
+
+
+
